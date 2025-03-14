b729cd717997f3d1a48fc5568459f87076ec75d1
Kevins Markuss Kanalis <KevinsKanalis@gmail.com>
2025-03-14 23:10:18 +0200

    fixed imports?

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-  import Navbar from "/src/components/Navbar.vue";
-  import Footer from "/src/components/Footer.vue";
+  import Navbar from "./components/Navbar.vue";
+  import Footer from "./components/Footer.vue";
   
 </script>
 
